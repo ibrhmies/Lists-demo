cd59d27971493c18b141f8db6bc85014ce2b2a16
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-11-01 12:53:29 +0300

    Python-Lists-demo
    Python'da liste metotları uygulama

---
+++ lists-demo.py
@@ -0,0 +1,67 @@
+teller = ["Samsung S5","Samsung S6","Samsung S7","Samsung S8"]
+
+#1->Liste kaç elemanlıdır?
+#Çözüm
+#sonuc = len(teller)
+#print(sonuc)
+
+#2->Listenin ilk ve son elemanı nedir?
+#çözüm
+#sonuc = teller[0]
+#sonuc = teller[3]
+#print(sonuc)
+
+#3-> samsung s5 değerini samsung s6 ile değiştirin 
+#çözüm
+#teller[0] = "Samsung S6"
+#print(teller)
+
+
+
+#4-> samsung s6 listenin bir elemanı mıdır?
+#çözüm
+#if "Samsung S6" in teller:
+    #print("True")
+
+
+#5-> listenin -3 indexindeki terim nedir?
+#çözüm 
+#sonuc = teller[-3]
+#print(sonuc)
+
+
+#6->listenin ilk iki elemanını alın
+#çözüm
+#sonuc = teller[0:2]
+#print(sonuc)
+
+
+#7->listenin son iki elemanı yerine iphone 5 ve iphone 6 ekleyin
+#çözüm
+#teller[0] = "iphone 5"
+#print(teller)
+#teller[1] = "iphone 6"
+#print(teller)
+
+#8->listenin üstüne iphone x ve iphone xr değerlerini ekleyin
+#çözüm
+#sonuc = teller + ["iphone x","iphone xr"]
+#print(sonuc)
+
+#9-> listenin son elemanını silin 
+#çözüm 
+#del teller[3]
+#print(teller)
+
+#10-> liste elemanlarını tersten yazdırınız
+#çözüm 
+#sonuc = teller[::-1]
+#print(sonuc)
+
+#11-> liste elemanlarını ekrana yazdırınız
+#çözüm
+#for x in teller:
+    #print(x)
+
+
+    
